9cc0187765d8432d6b9d47ae9ef012ea3d53e89a
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2023-09-12 03:12:23 +0530

    Landing page styles done.

--- src/Components/AddDetails/AddDetails.jsx
+++ src/Components/AddDetails/AddDetails.jsx
@@ -1,74 +1,78 @@
-
 const AddDetails = () => {
-  return (
-    <div>
-        <form action="POST">
-            <div>
-                <h5>Personal Details</h5>
-                <div style={{display:"flex"}}>
-                    <div>
-                        <label htmlFor="empID">Employee ID</label>
-                        <input type="text" placeholder="Employee ID" id="empID"/>
-                    </div>
-                    <div>
-                        <label htmlFor="fName">First Name</label>
-                        <input type="text" id="fName" placeholder="First Name"/>
-                    </div>
-                    <div>
-                        <label htmlFor="lName">Last Name</label>
-                        <input type="text" id="lName" placeholder="Last Name"/>
-                    </div>
-                    
-                </div>
-                <div style={{display:"flex"}} >
-                    <div>
-                        <label htmlFor="Email">Your E-mail</label>
-                        <input type="email" id="Email" placeholder="Your E-mail"/>
-                    </div>
-                    <div>
-                        <label htmlFor="pNo">Telephone</label>
-                        <input type="text"  id="pNo" placeholder="Telephone"/>
-                    </div>
-                    <div>
-                        <label htmlFor="doj">Joining Date:</label>
-                        <input type="text" id="doj"/>
-                    </div>
-                </div>
+    return (
+      <div style={{ backgroundColor: "gray", display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <form style={{ backgroundColor: "white", width: "50%", padding: "20px", borderRadius: "8px" }}>
+          <div>
+            <h5>Personal Details</h5>
+            <div style={{ display: "flex" }}>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="empID">Employee ID</label>
+                <input type="text" placeholder="Employee ID" id="empID" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="fName">First Name</label>
+                <input type="text" id="fName" placeholder="First Name" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="lName">Last Name</label>
+                <input type="text" id="lName" placeholder="Last Name" style={{ width: "100%" }} />
+              </div>
             </div>
-            <hr></hr>
-            <div>
-                <h5>Employment Details</h5>
-                
-
-                
-                <div style={{display:"flex"}}>
-                    <label htmlFor="depId">Department ID</label>
-                    <input type="text" id="depId" placeholder="Department ID" />
-
-                    <label htmlFor="depName">Department Name</label>
-                    <input type="text" id="depName" placeholder="Department Name" />
-
-                    <label htmlFor="ManagerName">ManagerName</label>
-                    <input type="text" id="ManagerName" placeholder="Manager Name" />
-                </div>
-                <div style={{display:"flex"}}>
-                    <label htmlFor="mngEmail">Manager Email</label>
-                    <input type="text" id="mngEmail" placeholder="Manager Email" />
-
-                    <label htmlFor="positionID">Position ID</label>
-                    <input type="text" id="positionID" placeholder="Position ID" />
-
-                    <label htmlFor="positionTitle">Position Title</label>
-                    <input type="text" id="positionTitle" placeholder="Position Title" />
-                </div>
+            <div style={{ display: "flex" }}>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="Email">Your E-mail</label>
+                <input type="email" id="Email" placeholder="Your E-mail" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="pNo">Telephone</label>
+                <input type="text" id="pNo" placeholder="Telephone" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="doj">Joining Date:</label>
+                <input type="text" id="doj" style={{ width: "100%" }} />
+              </div>
             </div>
-            <div>                
-                <button>Close</button>
-                <button>Save Details</button>
+          </div>
+          <hr style={{ margin: "20px 0" }} />
+          <div>
+            <h5>Employment Details</h5>
+            <div style={{ display: "flex" }}>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="depId">Department ID</label>
+                <input type="text" id="depId" placeholder="Department ID" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="depName">Department Name</label>
+                <input type="text" id="depName" placeholder="Department Name" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="ManagerName">Manager Name</label>
+                <input type="text" id="ManagerName" placeholder="Manager Name" style={{ width: "100%" }} />
+              </div>
             </div>
+            <div style={{ display: "flex" }}>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="mngEmail">Manager Email</label>
+                <input type="text" id="mngEmail" placeholder="Manager Email" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="positionID">Position ID</label>
+                <input type="text" id="positionID" placeholder="Position ID" style={{ width: "100%" }} />
+              </div>
+              <div style={{ flex: 1 }}>
+                <label htmlFor="positionTitle">Position Title</label>
+                <input type="text" id="positionTitle" placeholder="Position Title" style={{ width: "100%" }} />
+              </div>
+            </div>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <button style={{ backgroundColor: "red", color: "white", border: "none", padding: "10px 20px", borderRadius: "4px" }}>Close</button>
+            <button style={{ color: "white", border: "none", padding: "10px 20px", borderRadius: "4px" }}>Save Details</button>
+          </div>
         </form>
-    </div>
-  )
-}
-
-export default AddDetails
+      </div>
+    )
+  }
+  
+  export default AddDetails;
+  
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import AddDetails from "../Components/AddDetails/AddDetails"
 const LandingPage = () => {
   return (
     <div>
-        <div>
+        <div style={{textAlign:"center"}}>
             <button>Add Your Details Here</button>
             <button>Edit My Profile</button>
             <button>Logout</button>
